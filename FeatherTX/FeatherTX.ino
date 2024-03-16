125fff3153346d5176962cc5889d33303175f28e
tiedyefreak22 <olivine8910@gmail.com>
2024-03-15 20:41:14 -0600

    combining tx and rx files

---
+++ FeatherTX/FeatherTX.ino
@@ -0,0 +1,99 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define RFM95_CS    8
+#define RFM95_INT   3
+#define RFM95_RST   4
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+int led = LED_BUILTIN;
+int analogPin = A1; // sensor signal pin
+int val = 0;  // variable to store the value read
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+void setup() {
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  
+  // set LED to be an output pin
+  pinMode(led, OUTPUT);
+
+  Serial.begin(115200);
+  while (!Serial) delay(1);
+  delay(100);
+
+  Serial.println("Feather LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+}
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+void loop() {
+  delay(10000); // Wait 1 second between transmits, could also 'sleep' here!
+  Serial.println("Transmitting..."); // Send a message to rf95_server
+  digitalWrite(led, HIGH);    // turn the LED on
+
+  val = analogRead(analogPin);
+  char radiopacket[20];
+  sprintf(radiopacket, "%d", val);
+  itoa(packetnum++, radiopacket+13, 10);
+  Serial.print("Sending "); Serial.println(radiopacket);
+  radiopacket[19] = 0;
+
+  Serial.println("Sending...");
+  delay(10);
+  rf95.send((uint8_t *)radiopacket, 20);
+
+  Serial.println("Waiting for packet to complete...");
+  delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  Serial.println("Waiting for reply...");
+  if (rf95.waitAvailableTimeout(5000)) {
+    // Should be a reply message for us now
+    if (rf95.recv(buf, &len)) {
+      Serial.print("Got reply: ");
+      digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+    } else {
+      Serial.println("Receive failed");
+    }
+  } else {
+    Serial.println("No reply, is there a listener around?");
+  }
+
+}
