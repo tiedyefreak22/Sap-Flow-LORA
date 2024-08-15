7539d8918c2892ee5a980332cbeeb8fe9e5fc5e6
tiedyefreak22 <olivine8910@gmail.com>
2024-08-15 13:50:29 -0600

    Implemented datalogging on TX2 file

--- Feather/FeatherTX2/FeatherTX2.ino
+++ Feather/FeatherTX2/FeatherTX2.ino
@@ -2,17 +2,21 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 #include <RHReliableDatagram.h>
+#include <SD.h>
 
 #define RFM95_CS    8
 #define RFM95_INT   3
 #define RFM95_RST   4
+#define LED_RED     LED_BUILTIN
+#define SD_CS       10
 
 #define CLIENT_ADDRESS 1
 #define SERVER_ADDRESS 2
 
+// Should these be switched to #define?
 int led = LED_BUILTIN;
-int sense1 = A0; // sensor signal pin1
-int sense2 = A2; // sensor signal pin
+int sense1 = A0; // sensor signal pin 1
+int sense2 = A2; // sensor signal pin 2
 float val1 = 0.0;  // variable to store the value read
 float val2 = 0.0;  // variable to store the value read
 
@@ -22,6 +26,26 @@ float val2 = 0.0;  // variable to store the value read
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 RHReliableDatagram manager(rf95, CLIENT_ADDRESS);
+
+File logfile;
+
+// blink out an error code
+void error(uint8_t errnum) {
+  while(1) {
+    uint8_t i;
+    for (i=0; i<errnum; i++) {
+      digitalWrite(LED_RED, HIGH);
+      delay(100);
+      digitalWrite(LED_RED, LOW);
+      delay(100);
+      yield();
+    }
+    for (i=errnum; i<10; i++) {
+      delay(200);
+      yield();
+    }
+  }
+}
 
 void setup() {
   pinMode(led, OUTPUT);
@@ -33,6 +57,7 @@ void setup() {
 
   Serial.println("Feather LoRa TX Test!");
 
+  // RF95 Initialization
   // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(100);
@@ -63,6 +88,38 @@ void setup() {
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
+
+  // SD Card Initialization
+  // see if the card is present and can be initialized:
+  if (!SD.begin(SD_CS)) {
+    Serial.println("Card init. failed!");
+    error(2);
+  }
+  
+  Serial.println("SD card OK");
+  File root = SD.open("/");
+  printDirectory(root, 0);
+  
+  char filename[15];
+  strcpy(filename, "/ANALOG00.TXT");
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[7] = '0' + i/10;
+    filename[8] = '0' + i%10;
+    // create if does not exist, do not open existing, write, sync after write
+    if (! SD.exists(filename)) {
+      break;
+    }
+  }
+
+  logfile = SD.open(filename, FILE_WRITE);
+  if( ! logfile ) {
+    Serial.print("Couldnt create "); 
+    Serial.println(filename);
+    error(3);
+  }
+  Serial.print("Writing to "); 
+  Serial.println(filename);
+  Serial.println("Ready!");
 }
 
 int16_t counter = 0;  // packet counter, we increment per xmission
@@ -72,6 +129,12 @@ void loop() {
   delay(5000);
   
   val1 = analogRead(sense1);
+  // Read the data on pin A0 and log it to SD card and to serial
+  logfile.print("Sense1 = "); logfile.println(val1);
+  Serial.print("Sense1 = "); Serial.println(val1);
+
+  // save the output!
+  logfile.flush();
 
   // Gymnastics required to get analogRead val into float
   char radiopacket[20] = {' '};
@@ -110,4 +173,29 @@ void loop() {
 	delay(500);
 
   digitalWrite(led, LOW);    // turn the LED off
+}
+
+void printDirectory(File dir, int numTabs) {
+   while(true) {
+     
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
+  
 }
