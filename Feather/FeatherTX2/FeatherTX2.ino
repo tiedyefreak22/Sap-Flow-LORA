f7bf8f2a083b1d7b51c2b977f9471eb136d7cc0d
tiedyefreak22 <olivine8910@gmail.com>
2024-09-30 13:33:57 -0600

    Added time constraints and attempts limitations

--- Feather/FeatherTX2/FeatherTX2.ino
+++ Feather/FeatherTX2/FeatherTX2.ino
@@ -1,7 +1,7 @@
 // f1 MAC: 98:76:B6:12:99:a5
-#include <SPI.h>
-#include <RH_RF95.h>
 #include <RHReliableDatagram.h>
+#include <RH_RF95.h>
+#include <SPI.h>
 #include <SD.h>
 
 #define RFM95_CS    8
@@ -19,6 +19,7 @@ int sense1 = A0; // sensor signal pin 1
 int sense2 = A2; // sensor signal pin 2
 float val1 = 0.0;  // variable to store the value read
 float val2 = 0.0;  // variable to store the value read
+int REPORT_INT = 30.0; // interval at which to record and report data
 
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
@@ -29,56 +30,40 @@ RHReliableDatagram manager(rf95, CLIENT_ADDRESS);
 
 File logfile;
 
-// blink out an error code
-void error(uint8_t errnum) {
-  while(1) {
-    uint8_t i;
-    for (i=0; i<errnum; i++) {
-      digitalWrite(LED_RED, HIGH);
-      delay(100);
-      digitalWrite(LED_RED, LOW);
-      delay(100);
-      yield();
-    }
-    for (i=errnum; i<10; i++) {
-      delay(200);
-      yield();
-    }
-  }
-}
-
 void setup() {
   pinMode(led, OUTPUT);
   pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
-
-  Serial.begin(115200);
-  delay(2000);
-
-  Serial.println("Feather LoRa TX Test!");
 
   // RF95 Initialization
   // manual reset
+  digitalWrite(RFM95_RST, HIGH);
   digitalWrite(RFM95_RST, LOW);
   delay(100);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
 
+  Serial.begin(9600);
+  while(!Serial);
+
+  // if (!manager.init())
+	// 	Serial.println("Manager init failed");
+  //   error(2);
+  //   while(1);
+  // Serial.println("Manager init OK!");
+
   while (!rf95.init()) {
     Serial.println("LoRa radio init failed");
     Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    error(1);
     while (1);
   }
   Serial.println("LoRa radio init OK!");
 
-  if (!manager.init())
-		Serial.println("init failed");
-  Serial.println("Manager init OK!");
-
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
-    while (1);
+    error(3);
+    while(1);
   }
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
@@ -93,10 +78,11 @@ void setup() {
   // see if the card is present and can be initialized:
   if (!SD.begin(SD_CS)) {
     Serial.println("Card init. failed!");
-    error(2);
+    error(4);
+    while(1);
   }
-  
   Serial.println("SD card OK");
+
   File root = SD.open("/");
   printDirectory(root, 0);
   
@@ -115,7 +101,8 @@ void setup() {
   if( ! logfile ) {
     Serial.print("Couldnt create "); 
     Serial.println(filename);
-    error(3);
+    error(5);
+    while(1);
   }
   Serial.print("Writing to "); 
   Serial.println(filename);
@@ -124,55 +111,105 @@ void setup() {
 
 int16_t counter = 0;  // packet counter, we increment per xmission
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+unsigned long start = 0;
 
+// Device will record values every REPORT_INT and
+// attempt to make 30 reports at random intervals
+// in that time if it doesn't hear a response
 void loop() {
-  delay(5000);
+  delay(100);
+  unsigned long end = millis();
+  unsigned long duration = end - start;
+
+  if (duration / 1000 >= REPORT_INT) {
+    start = end;
+    val1 = analogRead(sense1);
+    // Read the data on pin A0 and log it to SD card and to serial
+    logfile.print("Sense1 = "); logfile.println(val1);
+    Serial.print("Sense1 = "); Serial.println(val1);
+
+    // save the output!
+    logfile.flush();
+
+    // Gymnastics required to get analogRead val into float
+    char radiopacket[20] = {' '};
+    int val1_int = (int) val1;
+    float val1_float = (abs(val1) - abs(val1_int)) * 100000;
+    int val1_fra = (int)val1_float;
+    sprintf(radiopacket, "%d.%d", val1_int, val1_fra);
+    
+    itoa(counter++, radiopacket+13, 10);
+    bool reply_flag = false;
+
+    int attempts = 1;
+    while ((!reply_flag) && (attempts < 30) && (((millis() - start) / 1000) < REPORT_INT)) {
+      int rand_time = rand() % 5 + 1;
+      Serial.print("Attempt "); Serial.println(attempts);
+      Serial.print("Sending "); Serial.println(radiopacket);
+      digitalWrite(led, HIGH);    // turn the LED on
+
+      delay(10);
+    // =============================== BASIC TRANSMISSION ===============================  
+      rf95.send((uint8_t *)radiopacket, strlen(radiopacket)+1);
+
+      Serial.println("Waiting for packet to complete...");
+      delay(10);
+      rf95.waitPacketSent();
+      // Now wait for a reply
+      uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+      uint8_t len = sizeof(buf);
+      
+      Serial.println("Waiting for reply...");
   
-  val1 = analogRead(sense1);
-  // Read the data on pin A0 and log it to SD card and to serial
-  logfile.print("Sense1 = "); logfile.println(val1);
-  Serial.print("Sense1 = "); Serial.println(val1);
+      if (rf95.waitAvailableTimeout(5000)) {
+        // Should be a reply message for us now
+        if (rf95.recv(buf, &len)) {
+          Serial.print("Got reply: ");
+          Serial.println((char*)buf);
+          Serial.print("RSSI: ");
+          Serial.println(rf95.lastRssi(), DEC);
+          reply_flag = true;
+        } else {
+          Serial.println("Receive failed");
+          break;
+        }
+      } else {
+        Serial.println("No reply, is there a listener around?");
+        attempts += 1;
+        digitalWrite(led, LOW);    // turn the LED off
+        delay(rand_time * 1000);
+      }
+    // =============================== END BASIC TRANSMISSION ===============================
 
-  // save the output!
-  logfile.flush();
+    // =============================== DATAGRAM TRANSMISSION ===============================
+    // Serial.println("Sending to rf95_reliable_datagram_server");
 
-  // Gymnastics required to get analogRead val into float
-  char radiopacket[20] = {' '};
-  int val1_int = (int) val1;
-  float val1_float = (abs(val1) - abs(val1_int)) * 100000;
-  int val1_fra = (int)val1_float;
-  sprintf(radiopacket, "%d.%d", val1_int, val1_fra);
-  
-  itoa(counter++, radiopacket+13, 10);
-  Serial.print("Sending "); Serial.println(radiopacket);
-  digitalWrite(led, HIGH);    // turn the LED on
+    // // Send a message to manager_server
+    // snprintf((char *)buf, sizeof(buf), "to server counter=%d", ++counter);
 
-  delay(10);
-  Serial.println("Sending to rf95_reliable_datagram_server");
-
-	// Send a message to manager_server
-	snprintf((char *)buf, sizeof(buf), "to server counter=%d", ++counter);
-
-	if (manager.sendtoWait(buf, strlen((char *)buf), SERVER_ADDRESS))
-	{
-		// Now wait for a reply from the server
-		uint8_t len = sizeof(buf);
-		uint8_t from;
-		if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
-		{
-			buf[len] = 0;
-			Serial.printf("got reply from 0x%02x rssi=%d %s", from, rf95.lastRssi(), (char *) buf);
-		}
-		else
-		{
-			Serial.println("No reply, is rf95_reliable_datagram_server running?");
-		}
-	}
-	else
-		Serial.println("sendtoWait failed");
-	delay(500);
-
-  digitalWrite(led, LOW);    // turn the LED off
+    // if (manager.sendtoWait(buf, strlen((char *)buf), SERVER_ADDRESS))
+    // {
+    // 	// Now wait for a reply from the server
+    // 	uint8_t len = sizeof(buf);
+    // 	uint8_t from;
+    // 	if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
+    // 	{
+    // 		buf[len] = 0;
+    // 		Serial.printf("got reply from 0x%02x rssi=%d %s", from, rf95.lastRssi(), (char *) buf);
+    // 	}
+    // 	else
+    // 	{
+    // 		Serial.println("No reply, is rf95_reliable_datagram_server running?");
+    // 	}
+    // }
+    // else
+    // 	Serial.println("sendtoWait failed");
+    //   error(6);
+    // delay(500);
+    // =============================== END DATAGRAM TRANSMISSION ===============================
+    }
+    digitalWrite(led, LOW);    // turn the LED off
+  }
 }
 
 void printDirectory(File dir, int numTabs) {
@@ -198,4 +235,22 @@ void printDirectory(File dir, int numTabs) {
      entry.close();
    }
   
+}
+
+// blink out an error code
+void error(uint8_t errnum) {
+  while(1) {
+    uint8_t i;
+    for (i=0; i<errnum; i++) {
+      digitalWrite(LED_RED, HIGH);
+      delay(200);
+      digitalWrite(LED_RED, LOW);
+      delay(200);
+      yield();
+    }
+    for (i=errnum; i<10; i++) {
+      delay(300);
+      yield();
+    }
+  }
 }
