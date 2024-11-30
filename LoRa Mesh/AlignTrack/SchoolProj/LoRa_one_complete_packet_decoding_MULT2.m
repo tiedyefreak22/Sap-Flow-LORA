c020cb671e5c1b7722c645612452b14f25eb8a94
tiedyefreak22 <olivine8910@gmail.com>
2024-11-30 16:16:59 -0700

    Split out more functions

--- LoRa Mesh/AlignTrack/SchoolProj/LoRa_one_complete_packet_decoding_MULT2.m
+++ LoRa Mesh/AlignTrack/SchoolProj/LoRa_one_complete_packet_decoding_MULT2.m
@@ -64,56 +64,16 @@ trials = 20;
 
 for trial = 1:1:trials
     trial
-    while true
-        try
-            payload_offset1 = round(Fs * (randi([round(offset_time / 2), offset_time]) / 1000)); % samples
-            payload_offset2 = round(Fs * (randi([round(offset_time / 2), offset_time]) / 1000)); % offset from second signal, not first
-
-            switch find([numel(symbols1), numel(symbols2), numel(symbols3)] == max(max([numel(symbols1), numel(symbols2), numel(symbols3)])), 1, 'first')
-                case 1
-                    diff = (max(max([numel(symbols1), numel(symbols2), numel(symbols3)])) - min(min([numel(symbols1), numel(symbols2), numel(symbols3)]))) * Ts * Fs;
-                    signalIQ1 = zeros(numel(phy.modulate(symbols1)) + payload_offset1 + payload_offset2 - diff, 1);
-                    signalIQ2 = signalIQ1;
-                    signalIQ3 = signalIQ1;
-                case 2
-                    diff = (max(max([numel(symbols1), numel(symbols2), numel(symbols3)])) - min(min([numel(symbols1), numel(symbols2), numel(symbols3)]))) * Ts * Fs;
-                    signalIQ2 = zeros(numel(phy.modulate(symbols2)) + payload_offset1 + payload_offset2 - diff, 1);
-                    signalIQ1 = signalIQ2;
-                    signalIQ3 = signalIQ2;
-                case 3
-                    diff = (max(max([numel(symbols1), numel(symbols2), numel(symbols3)])) - min(min([numel(symbols1), numel(symbols2), numel(symbols3)]))) * Ts * Fs;
-                    signalIQ3 = zeros(numel(phy.modulate(symbols3)) + payload_offset1 + payload_offset2 - diff, 1);
-                    signalIQ1 = signalIQ3;
-                    signalIQ2 = signalIQ3;
-                otherwise
-                    error("Can't determine max signal length");
-            end
-
-            signalIQ1(1:numel(phy.modulate(symbols1))) = phy.modulate(symbols1);
-            signalIQ2(payload_offset1 + 1:numel(phy.modulate(symbols2)) + payload_offset1) = phy.modulate(symbols2);
-            signalIQ3(payload_offset1 + payload_offset2 + 1:numel(phy.modulate(symbols3)) + payload_offset1 + payload_offset2) = phy.modulate(symbols3);
-            signalIQtotal = signalIQ1 + signalIQ2 + signalIQ3;
-            break
-        end
-    end
-
-    % Add zero leader
-    initial_offset = Fs * (randi([1, 10]) / 1000);
-    signalIQtotal = [zeros(initial_offset, 1); signalIQtotal];
+    % signal recieved at LoRa gateway
+    received_signal = send_receive_signal(offset_time, symbols1, symbols2, symbols3, Fs, Ts, phy, noise_sigma);
 
     % spectrogram for one LoRa packet
     if trial == trials
         figure
-        spectrogram(signalIQtotal, 1000, 0, 1000, Fs, 'yaxis', 'centered')
+        spectrogram(received_signal, 1000, 0, 1000, Fs, 'yaxis', 'centered')
         ylim([-BW / 2000000, BW / 2000000])
         saveas(gcf,'LoRa_Spectrogram.png')
     end
-
-    % AWGN noise
-    noise = noise_sigma * randn(length(signalIQtotal), 1);
-
-    % signal recieved at LoRa gateway
-    received_signal = signalIQtotal + noise;
 
 %% END Symbol Generation
 %% =============================================================================================
@@ -122,83 +82,35 @@ for trial = 1:1:trials
     for window = 1:1:length(windows)
         tStart = tic;
 
-        % Cross-Correlation for Preamble Detection
-        ref_upchirp = phy.chirp(true, SF, BW, Fs, 0, cfo, 0);
-        chirp_len = length(ref_upchirp);
-        k = BW / (chirp_len / Fs); % Chirp rate
-        correlation = abs(xcorr(received_signal, ref_upchirp));
-        [pks, locs] = findpeaks(correlation, 'MinPeakHeight', 0.5e4);
         i = 1;
         j = 1;
-        shift(i) = locs(1) - length(received_signal);
+        shift = [];
+        final_shift = [];
+        decoded_messages = [];
+        [shift(i), chirp_len, k] = cross_corr(SF, BW, Fs, cfo, received_signal, phy);
         final_shift(j) = shift(i);
         while true
             try
                 [decoded_message, ~] = phy.demodulate(received_signal(final_shift(j):end));
                 decoded_messages = {decoded_message(:, 1).'};
                 break
-            catch
-                while true
-                    try
-                        payload_offset1 = round(Fs * (randi([round(offset_time / 2), offset_time]) / 1000)); % samples
-                        payload_offset2 = round(Fs * (randi([round(offset_time / 2), offset_time]) / 1000)); % offset from second signal, not first
-            
-                        switch find([numel(symbols1), numel(symbols2), numel(symbols3)] == max(max([numel(symbols1), numel(symbols2), numel(symbols3)])), 1, 'first')
-                            case 1
-                                diff = (max(max([numel(symbols1), numel(symbols2), numel(symbols3)])) - min(min([numel(symbols1), numel(symbols2), numel(symbols3)]))) * Ts * Fs;
-                                signalIQ1 = zeros(numel(phy.modulate(symbols1)) + payload_offset1 + payload_offset2 - diff, 1);
-                                signalIQ2 = signalIQ1;
-                                signalIQ3 = signalIQ1;
-                            case 2
-                                diff = (max(max([numel(symbols1), numel(symbols2), numel(symbols3)])) - min(min([numel(symbols1), numel(symbols2), numel(symbols3)]))) * Ts * Fs;
-                                signalIQ2 = zeros(numel(phy.modulate(symbols2)) + payload_offset1 + payload_offset2 - diff, 1);
-                                signalIQ1 = signalIQ2;
-                                signalIQ3 = signalIQ2;
-                            case 3
-                                diff = (max(max([numel(symbols1), numel(symbols2), numel(symbols3)])) - min(min([numel(symbols1), numel(symbols2), numel(symbols3)]))) * Ts * Fs;
-                                signalIQ3 = zeros(numel(phy.modulate(symbols3)) + payload_offset1 + payload_offset2 - diff, 1);
-                                signalIQ1 = signalIQ3;
-                                signalIQ2 = signalIQ3;
-                            otherwise
-                                error("Can't determine max signal length");
-                        end
-            
-                        signalIQ1(1:numel(phy.modulate(symbols1))) = phy.modulate(symbols1);
-                        signalIQ2(payload_offset1 + 1:numel(phy.modulate(symbols2)) + payload_offset1) = phy.modulate(symbols2);
-                        signalIQ3(payload_offset1 + payload_offset2 + 1:numel(phy.modulate(symbols3)) + payload_offset1 + payload_offset2) = phy.modulate(symbols3);
-                        signalIQtotal = signalIQ1 + signalIQ2 + signalIQ3;
-                        break
-                    end
-                end
-            
-                % Add zero leader
-                initial_offset = Fs * (randi([1, 10]) / 1000);
-                signalIQtotal = [zeros(initial_offset, 1); signalIQtotal];
+            catch            
+                % signal recieved at LoRa gateway
+                received_signal = send_receive_signal(offset_time, symbols1, symbols2, symbols3, Fs, Ts, phy, noise_sigma);
             
                 % spectrogram for one LoRa packet
                 if trial == trials
                     figure
-                    spectrogram(signalIQtotal, 1000, 0, 1000, Fs, 'yaxis', 'centered')
+                    spectrogram(received_signal, 1000, 0, 1000, Fs, 'yaxis', 'centered')
                     ylim([-BW / 2000000, BW / 2000000])
                     saveas(gcf,'LoRa_Spectrogram.png')
                 end
-            
-                % AWGN noise
-                noise = noise_sigma * randn(length(signalIQtotal), 1);
-            
-                % signal recieved at LoRa gateway
-                received_signal = signalIQtotal + noise;
+
                 tStart = tic;
 
-                % Cross-Correlation for Preamble Detection
-                ref_upchirp = phy.chirp(true, SF, BW, Fs, 0, cfo, 0);
-                chirp_len = length(ref_upchirp);
-                k = BW / (chirp_len / Fs); % Chirp rate
-                correlation = abs(xcorr(received_signal, ref_upchirp));
-                [pks, locs] = findpeaks(correlation, 'MinPeakHeight', 0.5e4);
                 i = 1;
                 j = 1;
-                shift(i) = locs(1) - length(received_signal);
+                [shift(i), chirp_len, k] = cross_corr(SF, BW, Fs, cfo, received_signal, phy);
                 final_shift(j) = shift(i);
             end
         end
